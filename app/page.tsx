3f156ef1a4270144c1af8cad80063396ae935e1e
고경필 <126846896+kokyungpil@users.noreply.github.com>
2026-04-28 01:09:40 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
     <div style={{color: "white", background: "black", height: "100vh"}}>
       <h1>🍉 마나수박 오픈</h1>
       <p>만화 사이트 테스트 중</p>
-
+         <h1>🍉 승용 안녕  ~~~ </h1>
        <p>만화 사이트 테스트 중</p>
 
-
-      <h1> 陆星伦真是个白痴。他的名字就叫巴博伦。</h1>
+      MIMI   MIMI
+   
       
     </div>
   );
